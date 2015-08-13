f337a725ed72c8516cdf9043cd7768ea1a465a50
Sebastian Thiel <byron@eusthielloan.local>
2015-08-13 08:04:00 +0200

    cargo + semantics change for points example
    Please note that the points instance is currently leaked by Ruby.
    It would still have to be determined how fiddle can take ownership of resources

--- points-and-lines/points.rb
+++ points-and-lines/points.rb
@@ -4,7 +4,7 @@ require "fiddle/import"
 module RustPoint
   extend Fiddle::Importer
 
-  dlload "./libpoints.dylib"
+  dlload "./target/release/libpoints.dylib"
 
   extern "Point* make_point(int, int)"
   extern "double get_distance(Point*, Point*)"
@@ -20,19 +20,5 @@ class WorkingPoint < Struct.new(:x, :y)
 
   def to_rust
     RustPoint.make_point(x, y)
-  end
-end
-
-# Violates Rust's ownership rules and confuses Rust's memory management.
-class FailingPoint < Struct.new(:x, :y)
-  attr_reader :rust_pointer
-
-  def initialize(*args)
-    super
-    @rust_pointer = RustPoint.make_point(x, y)
-  end
-
-  def self.distance(p1, p2)
-    RustPoint.get_distance(p1.rust_pointer, p2.rust_pointer)
   end
 end
--- points-and-lines/points.rs
+++ points-and-lines/points.rs
@@ -1,18 +1,20 @@
-#![feature(core)]
-#![allow(missing_copy_implementations)]
-
-use std::num::Int;
-use std::num::Float;
+use std::mem;
 
 pub struct Point { x: i32, y: i32 }
-struct Line  { p1: Point, p2: Point }
 
-impl Line {
-  pub fn length(&self) -> f64 {
-    let xdiff = self.p1.x - self.p2.x;
-    let ydiff = self.p1.y - self.p2.y;
+impl Point {
+  pub fn distance_to(&self, p: &Point) -> f64 {
+    let xdiff = self.x - p.x;
+    let ydiff = self.y - p.y;
     ((xdiff.pow(2) + ydiff.pow(2)) as f64).sqrt()
   }
+}
+
+impl Drop for Point {
+    fn drop(&mut self) {
+        use std::io::{Write, stderr};
+        writeln!(stderr(), "PKill {{ x: {}, y: {} }}", self.x, self.y).ok();
+    }
 }
 
 #[no_mangle]
@@ -22,5 +24,17 @@ pub extern "C" fn make_point(x: i32, y: i32) -> Box<Point> {
 
 #[no_mangle]
 pub extern "C" fn get_distance(p1: Box<Point>, p2: Box<Point>) -> f64 {
-    Line {p1: *p1, p2: *p2}.length()
+    let res = p1.distance_to(&p2);
+    mem::forget(p1);
+    mem::forget(p2);
+    res
 }
+
+#[test]
+fn point_distance() {
+    let p1 = Point { x: 1, y: 2 };
+    assert_eq!(p1.distance_to(&p1), 0.0);
+
+    let p2 = Point { x: 2, y: 2 };
+    assert_eq!(p1.distance_to(&p2), 1.0);
+}   
